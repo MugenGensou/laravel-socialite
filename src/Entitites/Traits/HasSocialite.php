3f8a523251f6340ba25ab758f6918fab469c8210
Aki <mj501738931@126.com>
2018-01-04 16:58:19 +0800

    Add get open id method. Rename migration file.

--- src/Entitites/Traits/HasSocialite.php
+++ src/Entitites/Traits/HasSocialite.php
@@ -10,4 +10,13 @@ trait HasSocialite
     {
         return $this->hasMany(SocialiteUser::class, 'user_id');
     }
+
+    /**
+     * @param string $provider
+     * @return null|string
+     */
+    public function getOpenId(string $provider): ? string
+    {
+        return $this->socialites()->where('provider', $provider)->pluck('open_id')->first();
+    }
 }
